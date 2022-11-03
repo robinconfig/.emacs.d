feef67c7a85c22d74156a3017ad7a2ee0ab4a068
RobinVanYang <hongyemosuo@163.com>
2022-11-03 16:49:57 +0800

    init

---
+++ arch_first_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function install_menlo_font() {
+    MENLO_FONT_DIR="/usr/share/fonts/menlo/"
+    sudo mkdir $MENLO_FONT_DIR
+
+    cd /tmp
+    git clone https://github.com/ueaner/fonts
+    cd fonts
+    sudo cp *.ttf $MENLO_FONT_DIR
+
+    cd $MENLO_FONT_DIR
+    sudo mkfontscale
+    sudo mkfontdir
+    fc-cache -f
+}
+
+function main() {
+    install_menlo_font
+}
+
+main
+
+
---
+++ init.el
@@ -0,0 +1,4 @@
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "emacs-config.org"
+                   user-emacs-directory))
