0bfd129ca894a1d3666f172b051ff64a2b89a167
RobinVanYang <hongyemosuo@163.com>
2022-11-08 13:20:14 +0800

    save

--- init.el
+++ init.el
@@ -1,4 +1,13 @@
+;;; init.el --- Load the full configuration
+;;; Commentary:
+
+;;; Code:
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "emacs-config.org"
                    user-emacs-directory))
+
+(provide 'init)
+
+;;; init.el ends here
