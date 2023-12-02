ec9bfeee73a800c0bba258fb38b3eff95d9e2a69
RobinVanYang <hongyemosuo@163.com>
2023-12-03 04:02:39 +0800

    将flycheck设置为只在编程模式才启用

--- lisp/init-hint.el
+++ lisp/init-hint.el
@@ -2,10 +2,12 @@
 ;; flycheck 是一个更「现代」的引擎，功能更多，呈现样式更丰富，同时和 lsp-mode 结合最好
 
 (use-package flycheck
+  :ensture t
   :init ;; 在 (require) 之前需要执行的
   (setq flycheck-emacs-lisp-load-path 'inherit)
   :config
-  (global-flycheck-mode))
+  (setq truncate-lines nil) ; 如果单行信息很长会自动换行
+  :hook (prog-mode . flycheck-mode))
 
 
 (provide 'init-hint)
