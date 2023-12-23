3e67aab245bb22271e862b02f7195a96a5dbe1bd
RobinVanYang <hongyemosuo@163.com>
2023-12-23 22:29:08 +0800

    修复ivy配置中C-x b时调用已删除api的问题

--- lisp/bbt-ivy.el
+++ lisp/bbt-ivy.el
@@ -22,8 +22,26 @@
                           ivy-rich-switch-buffer-align-virtual-buffer t
                           ivy-rich-path-style 'abbrev)
   :config
-  (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer))
+  (defun ivy-rich-switch-buffer-icon (candidate)
+    (with-current-buffer
+	(get-buffer candidate)
+      (let ((icon (all-the-icons-icon-for-mode major-mode)))
+	(if (symbolp icon)
+            (all-the-icons-icon-for-mode 'fundamental-mode)
+          icon))))
+  (setq ivy-rich-display-transformers-list
+	'(ivy-switch-buffer
+          (:columns
+           ((ivy-rich-switch-buffer-icon (:width 2))
+            (ivy-rich-candidate (:width 30))
+            (ivy-rich-switch-buffer-size (:width 7))
+            (ivy-rich-switch-buffer-indicators (:width 4 :face error :align right))
+            (ivy-rich-switch-buffer-major-mode (:width 12 :face warning))
+            (ivy-rich-switch-buffer-project (:width 15 :face success))
+            (ivy-rich-switch-buffer-path (:width (lambda (x) (ivy-rich-switch-buffer-shorten-path x (ivy-rich-minibuffer-width 0.3))))))
+           :predicate
+           (lambda (cand) (get-buffer cand)))))
+  )
 
 (use-package swiper
   :after ivy
