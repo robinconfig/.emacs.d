603eb778de7582e4ea447e01d7a7a250dd7ba507
RobinVanYang <hongyemosuo@163.com>
2023-12-29 22:51:41 +0800

    update asssembly language config

--- lisp/programming/bbt-asm.el
+++ lisp/programming/bbt-asm.el
@@ -1,10 +1,18 @@
 ;; -*- lexical-binding: t -*-
 
 ;;(setq debug-on-error t)
-(use-package masm-mode
-  :ensure t)
+;;(use-package masm-mode
+;;  :ensure t)
+(use-package x86-lookup
+  :ensure t
+  :config
+  (setq  x86-lookup-pdf "~/Desktop/325462-sdm-vol-1-2abcd-3abcd-4.pdf")
+  (global-set-key (kbd "C-h x") #'x86-lookup))
 
-(use-package nasm-mode)
+(use-package nasm-mode
+  :ensure t
+  :config
+  (add-hook 'asm-mode-hook 'nasm-mode))
 
 
 (provide 'programming/bbt-asm)
