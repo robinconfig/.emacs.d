d90b4711087c05e40749da74c004619312816a3e
RobinVanYang <thehappyone@163.com>
2025-05-22 00:01:26 +0800

    update

---
+++ lisp/flycheck_init-vcs.el
@@ -0,0 +1,72 @@
+;;; INIT-VCS --- 版本控制配置文件 -*- lexical-binding: t -*-
+;;
+;; Author: RobinVanYang <thehappyone@163.com>
+;; Copyright © 2023 RobinVanYang. all rights reserved.
+;; URL: robinvanyang.github.io
+;; Created at:  3 八月 2023
+;;
+;;; Commentary:
+;;
+;; git svn等有关版本管理的配置
+;;
+;;; Code:
+
+;;(setq debug-on-error t)
+
+;; 据说很好用,有待探究
+(use-package magit)
+
+
+;; 侧边栏显示改动状态
+;; 移除， 原因： 会导致win11平台鼠标上下移动(evil)卡顿
+;; (use-package git-gutter
+;;   :ensure t
+;;   ;; :hook
+;;   ;; (prog-mode . git-gutter-mode)
+;;   :config
+;;   (global-git-gutter-mode t)
+;;   (setq git-gutter:update-interval 0.02))
+
+(use-package diff-hl
+  :custom-face
+  (diff-hl-change ((t (:inherit custom-changed :foreground unspecified :background unspecified))))
+  (diff-hl-insert ((t (:inherit diff-added :background unspecified))))
+  (diff-hl-delete ((t (:inherit diff-removed :background unspecified))))
+  :bind (:map diff-hl-command-map
+         ("SPC" . diff-hl-mark-hunk))
+  :hook ((after-init . global-diff-hl-mode)
+         (after-init . global-diff-hl-show-hunk-mouse-mode)
+         (dired-mode . diff-hl-dired-mode))
+  :init (setq diff-hl-draw-borders nil)
+  :config
+  ;; Highlight on-the-fly, 启用此效果后在Mac下编辑vcs下文件的时候特别卡, 所以禁用掉
+  ;; (diff-hl-flydiff-mode 1)
+
+  ;; Set fringe style
+  (setq-default fringes-outside-margins t)
+
+  (with-no-warnings
+    (defun my-diff-hl-fringe-bmp-function (_type _pos)
+      "Fringe bitmap function for use as `diff-hl-fringe-bmp-function'."
+      (define-fringe-bitmap 'my-diff-hl-bmp
+        (vector (if openeyes/is-linux #b11111100 #b11100000))
+        1 8
+        '(center t)))
+    (setq diff-hl-fringe-bmp-function #'my-diff-hl-fringe-bmp-function)
+
+    (unless (display-graphic-p)
+      ;; Fall back to the display margin since the fringe is unavailable in tty
+      (diff-hl-margin-mode 1)
+      ;; Avoid restoring `diff-hl-margin-mode'
+      (with-eval-after-load 'desktop
+        (add-to-list 'desktop-minor-mode-table
+                     '(diff-hl-margin-mode nil))))
+
+    ;; Integration with magit
+    (with-eval-after-load 'magit
+      (add-hook 'magit-pre-refresh-hook #'diff-hl-magit-pre-refresh)
+      (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh))))
+
+
+(provide 'init-vcs)
+;;; init-vcs.el ends here
